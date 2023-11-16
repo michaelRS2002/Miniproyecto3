c38e1c7d2ed6f641a370d0450aed283fb67313b5
invitado <invitado@s1pc10.eisc.univalle.edu.co>
2023-11-16 17:43:11 -0500

    Empezando codigo

---
+++ Miniproyecto3/src/Modelo/Main.java
@@ -0,0 +1,17 @@
+
+package Modelo;
+import Vista.GUIMiniProyecto3;
+/**
+ *
+ * @author invitado
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       GUIMiniProyecto3 app = new GUIMiniProyecto3();
+    }
+    
+}
---
+++ Miniproyecto3/src/Vista/GUIMiniProyecto3.java
@@ -0,0 +1,52 @@
+package Vista;
+
+/*
+ *
+ * @author invitado
+ */
+import java.awt.*;
+import javax.swing.*;
+import static javax.swing.WindowConstants.EXIT_ON_CLOSE;
+
+public class GUIMiniProyecto3 extends JFrame {
+   
+    JLabel lNom;
+    JTextField jtPresen;
+    JPanel pGeneral;
+    
+    public GUIMiniProyecto3()
+    {
+        setTitle("Examenes Conectados");
+        setSize(500,500);
+        crearGUI1();
+        setVisible(true);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        
+    }
+    
+    public void crearGUI1()
+    {
+        lNom = new JLabel("<html>Bienvenido a Examenes Conectados<br>"
+                          +"tu plataforma para hacer examenes <br>"
+                          +"en grupos!!");
+        pGeneral = new JPanel(new GridLayout(2,1));
+        jtPresen = new JTextField();
+        
+        lNom.setHorizontalAlignment(SwingConstants.CENTER);
+        
+        Font forte = new Font("Comic Sans", Font.PLAIN, 20);
+        lNom.setFont(forte);
+        
+        
+               
+        //ubicacion de los elementos de la presentacion del programa
+        pGeneral.add(lNom);
+        pGeneral.add(jtPresen);
+        
+        pGeneral.setBorder(BorderFactory.createTitledBorder(BorderFactory.createLineBorder(Color.RED, 3)));
+        
+        //Organizar ventana
+        add(pGeneral);               
+    }
+}
+
