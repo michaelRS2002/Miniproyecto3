f7741bbb47c03d8dc3810d435fdff3ef33e22ff6
invitado <invitado@s1pc10.eisc.univalle.edu.co>
2023-11-16 18:46:30 -0500

    Menu hecho

--- Miniproyecto3/src/Vista/GUIMiniProyecto3.java
+++ Miniproyecto3/src/Vista/GUIMiniProyecto3.java
@@ -5,20 +5,22 @@ package Vista;
  * @author invitado
  */
 import java.awt.*;
+import java.util.HashSet;
 import javax.swing.*;
 import static javax.swing.WindowConstants.EXIT_ON_CLOSE;
 
 public class GUIMiniProyecto3 extends JFrame {
    
-    JLabel lNom;
+    JLabel lNom, lImagenMenu;
     JTextField jtPresen;
-    JPanel pGeneral;
-    JMenuItem inicio;
+    JPanel pGeneral, pMenu;
+    JMenuBar barra;
+    JMenuItem inicio, crearExamen, verExamenes, verInformes, realizarExamen;
     
     public GUIMiniProyecto3()
     {
         setTitle("Examenes Conectados");
-        setSize(500,500);
+        setSize(640,540);
         crearGUI1();
         setVisible(true);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
@@ -28,13 +30,28 @@ public class GUIMiniProyecto3 extends JFrame {
     public void crearGUI1()
     {
         //Panel Menu
-        inicio= new JMenuItem("Inicio");
+        barra = new JMenuBar();
+        inicio = new JMenuItem("Inicio");
+        inicio.setHorizontalAlignment(JMenuItem.CENTER);
+        crearExamen = new JMenuItem("Crear Examen");
+        crearExamen.setHorizontalAlignment(JMenuItem.CENTER);
+        verExamenes = new JMenuItem("Ver Examenes");
+        verExamenes.setHorizontalAlignment(JMenuItem.CENTER);
+        verInformes = new JMenuItem("Ver informes");
+        verInformes.setHorizontalAlignment(JMenuItem.CENTER);
+        realizarExamen = new JMenuItem("Realizar Examen");
+        realizarExamen.setHorizontalAlignment(JMenuItem.CENTER);
         
         lNom = new JLabel("<html>Bienvenido a Examenes Conectados<br>"
                           +"tu plataforma para hacer examenes <br>"
                           +"en grupos!!");
         pGeneral = new JPanel(new GridLayout(2,1));
         jtPresen = new JTextField();
+        lImagenMenu = new JLabel();
+        lImagenMenu.setIcon(new ImageIcon("src/Images/imagenMenu.png"));
+        lImagenMenu.setHorizontalAlignment(JLabel.CENTER);
+        
+        
         
         lNom.setHorizontalAlignment(SwingConstants.CENTER);
         
@@ -45,12 +62,20 @@ public class GUIMiniProyecto3 extends JFrame {
                
         //ubicacion de los elementos de la presentacion del programa
         pGeneral.add(lNom);
-        pGeneral.add(jtPresen);
-        
+        pGeneral.add(lImagenMenu);
+        barra.add(inicio);
+        barra.add(crearExamen);
+        barra.add(verExamenes);
+        barra.add(verInformes);
+        barra.add(realizarExamen);
+        setJMenuBar(barra);
         pGeneral.setBorder(BorderFactory.createTitledBorder(BorderFactory.createLineBorder(Color.RED, 3)));
         
         //Organizar ventana
-        add(pGeneral);               
+        add(pGeneral, BorderLayout.CENTER); 
+        //add(barra, BorderLayout.NORTH);
+        
+        
     }
 }
 
